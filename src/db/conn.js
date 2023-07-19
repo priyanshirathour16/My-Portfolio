2e3f003a832786b77a074e4ff3373c9ac91fb0ac
Priyanshi Rathour <115027616+priyanshirathour16@users.noreply.github.com>
2023-07-19 21:56:25 +0530

    Update conn.js

--- src/db/conn.js
+++ src/db/conn.js
@@ -1,6 +1,6 @@
 const mongoose = require("mongoose");
 
-mongoose.connect("mongodb://localhost:27017/employeeuser" , {
+mongoose.connect("mongodb://containers-us-west-155.railway.app:7692/employeeuser" , {
     useNewUrlParser:true,
     useUnifiedTopology:true
 })
